0d3b453a94c6b4d86aad663a7f6e2a16e012752f
Sreekar <gadasusreekar@gmail.com>
2021-11-22 21:34:10 +0530

    adding content

--- src/app/Header.js
+++ src/app/Header.js
@@ -2,16 +2,10 @@ import react, { Component } from 'react';
 import logo from '../app/assets/yellow-electric-lightning-bolt-icon.png';
 import logo1 from '../app/assets/icon_1.png';
 import * as Scroll from 'react-scroll';
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 import textSharerLogo from '../app/assets/text-sharer-logo.png';
 
 class Header extends Component{
-    scrollToTop() {
-    scroll.scrollToTop();
-    }
-    scrollToBottom() {
-        scroll.scrollToBottom();
-    }
     scrollTo() {
         scroll.scrollTo(500);
     }
