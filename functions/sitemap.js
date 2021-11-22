46aa92293abcebc0f2c875d24fe9fc37f9c518bb
Sreekar <gadasusreekar@gmail.com>
2021-11-22 13:25:32 +0530

    adding sitemap

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -2,8 +2,20 @@ exports.handler = function(event, context, callback) {
   var xml_content = [
     '<?xml version="1.0" encoding="UTF-8"?>',
     '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-    `<url><loc>http://text-sharer.netlify.com/</loc></url>`,
-    `<url><loc>http://text-sharer.netlify.com/privacy_policy</loc></url>`,
+    `<url><loc>http://text-sharer.netlify.com/</loc>
+      <lastmod>2021-11-22</lastmod>
+
+      <changefreq>monthly</changefreq>
+
+      <priority>0.8</priority>
+</url>`,
+    `<url><loc>http://text-sharer.netlify.com/privacy_policy</loc>
+      <lastmod>2021-11-22</lastmod>
+
+      <changefreq>monthly</changefreq>
+
+      <priority>0.8</priority>
+</url>`,
     "</urlset>"
   ];
 
